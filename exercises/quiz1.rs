65c98b5e8ca763f304f0867515f6c76c0542aa15
atomic22 <atomic22@outlook.com>
2025-01-18 16:57:04 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,11 +13,15 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
 // fn calculate_price_of_apples {
-
+fn calculate_price_of_apples(nums: u32) -> u32 {
+    if nums <= 40 {
+        nums * 2
+    } else {
+        nums
+    }
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
